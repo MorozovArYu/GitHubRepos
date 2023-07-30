b6d9810674db9743bbe74c225ce5b28691227060
Morozov Artem <morozowau@gmail.com>
2023-07-30 12:17:30 +0300

    Add test module

---
+++ app/src/test/kotlin/dev/icerock/education/githubrepos/ExampleTest.kt
@@ -0,0 +1,12 @@
+package dev.icerock.education.githubrepos
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun `test`(){
+        assertEquals(true,true)
+    }
+}
