6d8b84c79b85ec2041185a9b073ad9b4a18695d7
Morozov Artem <morozowau@gmail.com>
2023-08-03 16:50:17 +0300

    [TDD] Pass test's

--- app/src/main/kotlin/dev/icerock/education/githubrepos/domain/model/UserInfo.kt
+++ app/src/main/kotlin/dev/icerock/education/githubrepos/domain/model/UserInfo.kt
@@ -1,6 +1,17 @@
 package dev.icerock.education.githubrepos.domain.model
 
 sealed interface UserInfo {
-    data class Success(val ownerName: String) : UserInfo
-    class Error(val message: String) : UserInfo
+    fun <T> map(mapper: Mapper<T>): T
+    data class Success(val ownerName: String) : UserInfo {
+        override fun <T> map(mapper: Mapper<T>): T = mapper.map(this)
+    }
+
+    class Error(val message: String) : UserInfo {
+        override fun <T> map(mapper: Mapper<T>): T = mapper.map(this)
+    }
+
+    interface Mapper<T> {
+        fun map(userInfo: UserInfo.Success): T
+        fun map(userInfo: UserInfo.Error): T
+    }
 }
