e5341e6c8419c7d642652ccd92f964f375819a86
Morozov Artem <morozowau@gmail.com>
2023-08-03 16:10:13 +0300

    [TDD] Refacotor AuthUseCaseTest
    We don't need auto and manual modes, because cache data will
    be checked in presenter layer

--- app/src/test/kotlin/dev/icerock/education/githubrepos/domain/auth/AuthTest.kt
+++ app/src/test/kotlin/dev/icerock/education/githubrepos/domain/auth/AuthTest.kt
@@ -8,31 +8,16 @@ import org.junit.Test
 class AuthTest : AuthUseCaseTest() {
 
     @Test
-    fun `valid token - success manual auth`() = runTest {
-        val actual: UserInfo = authUseCase.auth(AuthUseCase.AuthType.Manual(TestUtils.VALID_TOKEN))
+    fun `valid token - success auth`() = runTest {
+        val actual: UserInfo = authUseCase.auth(TestUtils.VALID_TOKEN)
         val expected: UserInfo = TestUtils.USER_INFO_SUCCESS
         Assert.assertEquals(actual, expected)
     }
 
     @Test
-    fun `invalid token - error manual auth`() = runTest {
+    fun `invalid token - error auth`() = runTest {
         val actual: UserInfo =
-            authUseCase.auth(AuthUseCase.AuthType.Manual(TestUtils.INVALID_TOKEN))
-        val expected: UserInfo = TestUtils.USER_INFO_ERROR
-        Assert.assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `valid token - success auto auth`() = runTest {
-        val actual: UserInfo = authUseCase.auth()
-        val expected: UserInfo = TestUtils.USER_INFO_SUCCESS
-        Assert.assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `invalid token - error auto auth`() = runTest {
-        repository.setInvalidToken()
-        val actual: UserInfo = authUseCase.auth()
+            authUseCase.auth(TestUtils.INVALID_TOKEN)
         val expected: UserInfo = TestUtils.USER_INFO_ERROR
         Assert.assertEquals(actual, expected)
     }
